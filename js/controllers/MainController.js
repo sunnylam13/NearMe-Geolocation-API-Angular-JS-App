dcb4fc917c475e822c8138ba359ce634863c3fa2
Sunny Lam <sunny.lam@gmail.com>
2015-07-01 15:43:23 -0400

    setup the pop up map markers.  all of the locations were placed on the map.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,4 @@
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope','places', function($scope,places) {
   
   ////////////////////////////////////////////
   /////     CENTER ATTRIBUTE
@@ -22,24 +22,24 @@ app.controller('MainController', ['$scope', function($scope) {
   ////////////////////////////////////////////
   /////     MARKERS ATTRIBUTE
   ///////////////////////////////////////////////
-    /* 
-    * <leaflet> directive can take information about the map's markers through the markers attribute
-    * In the view, pass mapMarkers into the directive's markers attribute.
-    * View the result in the browser. The map has two markers that you can click on.
-    */
-   
-    $scope.mapMarkers = [
-      {
-        lat: 40.741389,
-        lng: -74.003056,
-        message: "111 Eighth Avenue"
-      },
-      {
-        lat: 40.7425,
-        lng: -74.006111,
-        message: "Chelsea Market"
-      }
-    ];
+
+  /* 
+  * use service 'places' instead of hard coding
+  * use the places service to fetch marker data from the Wikipedia API
+  * Create a new file js/vendor/helpers.js. 
+    * Copy all the code from this JavaScript file and paste it into your js/vendor/helpers.js. Then include it as as a new <script>. We'll use the geodataToMarkers() function to format the Wikipedia API's output so that we can use it in the map.
+  * Add 'places' into MainController as a dependency so that it's available to use.
+  * Then use places within the controller to asynchronously fetch the places data from the server, and store it into $scope.geodata.
+  * Use thegeodataToMarkers() function to format $scope.geodata, and store that output into $scope.mapMarkers, like this:
+    * $scope.mapMarkers = geodataToMarkers($scope.geodata);
+  * In the view, pass mapMarkers into the <leaflet> directive's markers attribute.
+  */
+
+  places.success(function(data) {
+    $scope.geodata = data;
+    $scope.mapMarkers = geodataToMarkers($scope.geodata);
+  });
+
   ///////////////////////////////////////////////
   /////     END MARKERS ATTRIBUTE
   ///////////////////////////////////////////////  
