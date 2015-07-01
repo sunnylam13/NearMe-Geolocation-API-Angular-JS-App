6df3855d159526075d138c3b14830ecc4551a3ba
Sunny Lam <sunny.lam@gmail.com>
2015-07-01 16:26:15 -0400

    Setup the about page for the project and routing.  Styled it.

--- js/app.js
+++ js/app.js
@@ -14,10 +14,17 @@
 * This version uses routing.
 *
 * Refactor the app to set up the controller MainController and the template views/main.html 
-* Make a new file views/main.html.
-Cut the <div class="main"< section from index.html, and paste it into views/main.html.
-* In index.html, replace the <div class="main"> with <div ng-view></div>.
-* In views/main.html, remove the ng-controller="MainController" from <div class="main">.
+	* Make a new file views/main.html.
+	Cut the <div class="main"< section from index.html, and paste it into views/main.html.
+	* In index.html, replace the <div class="main"> with <div ng-view></div>.
+	* In views/main.html, remove the ng-controller="MainController" from <div class="main">.
+*
+* In app.js, add another route. Map the URL /about to the controller AboutController and the template views/about.html.
+	* In js/controllers/AboutController.js, create a new controller named AboutController. Include it as a new <script> element.
+	* Create a template in views/about.html to display an About page. Here's what ours looks like. 
+		* https://s3.amazonaws.com/codecademy-content/projects/4/nearme-3/index.html#/about
+		* Write HTML in the template and CSS in css/main.css to create an About page for the app.
+	* Add a link to the About page that says "Start exploring". Set the href="#/" so it links back to the map.
 */
 
 var app = angular.module("NearMeApp",['leaflet-directive','ngRoute']);
@@ -27,16 +34,30 @@ var app = angular.module("NearMeApp",['leaflet-directive','ngRoute']);
 ///////////////////////////////////////////////
   app.config(function ($routeProvider) { 
     $routeProvider 
-      .when('/', { 
-        controller: 'MainController', 
-        templateUrl: 'views/main.html' 
-      }) 
-      // .when('/photos/:id',{
-      //   // maps the URL /photos/:id to the controller PhotoController and the template views/photo.html
-      //   // visit localhost... click on a photo. A detail page about that photo should appear
-      //   controller: 'PhotoController', 
-      //   templateUrl: 'views/photo.html' 
-      // })
+    	// ----------------------------------------
+    	// MAIN CONTROLLER ROUTE  ------------------
+    	// ----------------------------------------
+    		.when('/', { 
+    		  controller: 'MainController', 
+    		  templateUrl: 'views/main.html' 
+    		}) 
+    	// ----------------------------------------
+    	// END MAIN CONTROLLER ROUTE  ------------------
+    	// ----------------------------------------
+      
+    	// ----------------------------------------
+    	// ABOUT PAGE ROUTE  ------------------
+    	// ----------------------------------------
+    		.when('/about',{
+    		  // maps the URL /photos/:id to the controller PhotoController and the template views/photo.html
+    		  // visit localhost... click on a photo. A detail page about that photo should appear
+    		  controller: 'AboutController', 
+    		  templateUrl: 'views/about.html' 
+    		})
+    	// ----------------------------------------
+    	// END ABOUT PAGE ROUTE  ------------------
+    	// ----------------------------------------
+      
       .otherwise({ 
         redirectTo: '/' 
       }); 
---
+++ js/controllers/AboutController.js
@@ -0,0 +1,6 @@
+/* 
+* About page
+* don't forget to include this script in index.html
+* apparently if all you're doing is injecting html there's not much you need to code
+*/
+
